370dac35b84309adb074be9aecb1a8b8f8971b69
Olivia Bishop <olivia@fb.com>
2015-04-29 20:21:30 +0100

    single puzzle rendering

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,12 @@
  */
 'use strict';
 
+var PUZZLE_DATA = [
+  {date: 1430332677984, title: 'Burning Island Puzzle', text: 'A man is stranded on an island covered in forest. One day, when the wind is blowing from the west, lightning strikes the west end of the island and sets fire to the forest. The fire is very violent, burning everything in its path, and without intervention the fire will burn the whole island, killing the man in the process. There are cliffs around the island, so he cannot jump off. How can the man survive the fire? (There are no buckets or any other means to put out the fire).', solution: 'The man picks up a piece of wood and lights it from the fire on the west end of the island. He then quickly carries it near the east end of he island and starts a new fire. The wind will cause that fire to burn out the eastern end and he can then shelter in the burnt area. The man survives the fire, but dies of starvation, with all the food in the forest burnt.'},
+];
+
+var MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+
 var React = require('react-native');
 var {
   AppRegistry,
@@ -12,39 +18,78 @@ var {
   View,
 } = React;
 
+PUZZLE_DATA[0].title
+
+var prettyDate = function (unixtime) {
+  var date = new Date(unixtime)
+  var mm = MONTHS[date.getMonth()];
+  var dd = date.getDate().toString();
+  var yyyy = date.getFullYear().toString();
+
+  return mm + ' ' + dd + ', ' + yyyy;
+};
+
 var DailyPuzzle = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
+      <View style={styles.outer}>
+        <View style={styles.container}>
+          <Text style={styles.title}>
+            {PUZZLE_DATA[0].title}
+          </Text>
+          <Text style={styles.date}>
+            {prettyDate(PUZZLE_DATA[0].date)}
+          </Text>
+          <Text style={styles.text}>
+            {PUZZLE_DATA[0].text}
+          </Text>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.instructions}>
+            {'<-'} for more puzzles,{'\n'}
+            Swipe up for the solution.
+          </Text>
+        </View>
       </View>
     );
   }
 });
 
 var styles = StyleSheet.create({
+  outer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'flex-start',
+    backgroundColor: 'white',
+    padding: 20,
   },
-  welcome: {
+  footer: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: 'white',
+    padding: 20,
+  },
+  title: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: 'left',
+    marginBottom: 5,
+  },
+  date: {
+    fontSize: 12,
+    textAlign: 'left',
+    color: '#CFCFCF',
+    marginBottom: 20,
+  },
+  text: {
+    textAlign: 'left',
+    color: '#333333',
+    marginBottom: 5,
   },
   instructions: {
-    textAlign: 'center',
+    textAlign: 'left',
     color: '#333333',
     marginBottom: 5,
   },
